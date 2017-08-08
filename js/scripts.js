c02ec16f69c6dd9162d210e6f1d83fdcea0600fa
linda luu, snow vilay and tyler miller <tyler@>
2017-08-08 09:02:54 -0700

    toggle text, DOM

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
+
+
+  });
+  $(".clickable2").click(function() {
+  $("#sea-lion-show").fadeToggle();
+  $("#sea-lion-hide").fadeToggle();
+
+});
+
+});
